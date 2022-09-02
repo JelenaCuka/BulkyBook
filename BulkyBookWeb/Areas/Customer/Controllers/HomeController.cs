9349a6690231c99a4237eec69f9d5d139c81f3db
Jelena Cuka <cuka.jelena@gmail.com>
2022-09-02 18:32:46 +0200

    Added Product Details View

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -22,6 +22,16 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
             return View(productList);
         }
+        public IActionResult Details(int id)
+        {
+            ShoppingChart chartObj = new()
+            {
+                Count = 1,
+                Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,CoverType")
+            };
+                
+            return View(chartObj);
+        }
 
         public IActionResult Privacy()
         {
