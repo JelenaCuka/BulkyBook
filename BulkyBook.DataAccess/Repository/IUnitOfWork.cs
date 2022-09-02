6b8a298647a8c70df6ad3b5719f95d2e49abc8f1
Jelena Cuka <cuka.jelena@gmail.com>
2022-09-02 11:48:35 +0200

    Added Product Repository

--- BulkyBook.DataAccess/Repository/IUnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/IUnitOfWork.cs
@@ -10,6 +10,8 @@ namespace BulkyBook.DataAccess.Repository
     {
         ICategoryRepository Category { get; }
         ICoverTypeRepository CoverType { get; }
+        IProductRepository Product { get; }
+
         void Save();
     }
 }
