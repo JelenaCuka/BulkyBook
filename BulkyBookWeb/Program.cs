68040724e1faf3e077a07df43949cee71fb02c2a
Jelena Cuka <cuka.jelena@gmail.com>
2022-09-06 19:18:36 +0200

    Added Stripe secrets configuration

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using BulkyBook.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using BulkyBook.Utility;
+using Stripe;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,7 @@ builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("StripeSettings"));
 
 builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddDefaultTokenProviders() // options => options.SignIn.RequireConfirmedAccount = true
     .AddEntityFrameworkStores<ApplicationDbContext>();
@@ -40,7 +42,10 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseAuthentication();;
+
+StripeConfiguration.ApiKey = builder.Configuration.GetSection("StripeSettings:SecretKey").Get<string>();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
